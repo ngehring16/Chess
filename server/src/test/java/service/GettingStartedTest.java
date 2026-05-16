97bd61d0c02f730e4c10ab0e711ad50566d2a42f
nathangehring <nathanwgehring@gmail.com>
2026-05-16 11:58:57 -0600

    Created working positive and negative tests for GettingStarted methods.

--- server/src/test/java/service/GettingStartedTest.java
+++ server/src/test/java/service/GettingStartedTest.java
@@ -1,8 +1,6 @@
 package service;
 
-import chessrecords.AlreadyTakenException;
-import chessrecords.RegisterResult;
-import chessrecords.UserData;
+import chessrecords.*;
 import dataaccess.AuthDataAccess;
 import dataaccess.DataAccessException;
 import dataaccess.UserDataAccess;
@@ -13,19 +11,30 @@ class GettingStartedTest {
     private final AuthDataAccess authAccess = new AuthDataAccess();
     private final UserDataAccess userAccess = new UserDataAccess();
     private final GettingStarted starter = new GettingStarted(authAccess, userAccess);
+    private final UserData user = new UserData("ngehring", "password", "hello@gmail.com");
     @Test
-    void registerPositive() throws AlreadyTakenException, DataAccessException {
-        UserData user = new UserData("ngehring", "password", "hello@gmail.com");
+    void registerWithValidRequestPositive() throws AlreadyTakenException, DataAccessException {
         RegisterResult result = starter.register(user);
         Assertions.assertEquals(result.username(), user.username());
    }
    @Test
-   void registerNegative() {
+   void registerWithInvalidRequestNegative() {
         UserData user = new UserData(null, "null", "null");
         Assertions.assertThrows(DataAccessException.class, ()->{starter.register(user);});
    }
 
     @Test
-    void login() {
+    void loginWithAccurateAuthTokenPositive() throws DoesNotExistException, DoesNotMatchException, DataAccessException, AlreadyTakenException {
+        starter.register(user);
+        LoginRequest request = new LoginRequest("ngehring", "password");
+        RegisterResult result = starter.login(request);
+        Assertions.assertEquals(result.authToken(), authAccess.getAuth(result.authToken()).authToken());
+    }
+
+    @Test
+    void loginWithTheWrongPasswordNegative() throws DataAccessException, AlreadyTakenException {
+        starter.register(user);
+        LoginRequest request = new LoginRequest("ngehring", "incorrectPassword");
+        Assertions.assertThrows(DoesNotMatchException.class, ()->{starter.login(request);});
     }
 }
---
+++ server/src/test/java/service/IsLoggedInTest.java
@@ -0,0 +1,32 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IsLoggedInTest {
+
+    @Test
+    void isAuthorized() {
+    }
+
+    @Test
+    void validColors() {
+    }
+
+    @Test
+    void logout() {
+    }
+
+    @Test
+    void list() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void joinGame() {
+    }
+}
