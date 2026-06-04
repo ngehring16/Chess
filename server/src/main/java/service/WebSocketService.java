e2d8c857c3b0994c881f9c12034a027b68647ee1
nathangehring <nathanwgehring@gmail.com>
2026-06-04 17:34:22 -0600

    Creating more helper functions to send notifications for checkmate, stalemate, and check as well as the leave implimentation.

--- server/src/main/java/service/WebSocketService.java
+++ server/src/main/java/service/WebSocketService.java
@@ -28,31 +28,10 @@ public class WebSocketService {
         this.gameAccess = gameAccess;
     }
 
-    private String moveTranslator(ChessMove move){
-        String[] letters = { "A", "B", "C" ,"D", "E", "F", "G", "H"};
-        int row = move.getEndPosition().getRow();
-        int col = move.getEndPosition().getColumn();
-        return letters[col-1] + row;
-    }
-
-    private void isValid(String authToken, int gameID) throws Exception{
-        if (authAccess.getAuth(authToken) == null){
-            throw new DataAccessException("You are not authorized, please login.");
-        }
-        if (gameAccess.getGame(gameID) == null){
-            throw new DataAccessException("This game does not exist. Please pick a different game.");
-        }
-    }
-
-    private boolean moveIsValid(Collection<ChessMove> validMoves, ChessMove move){
-        return validMoves.contains(move);
-    }
-
     public void connect(Session session, int gameID, String authToken) throws Exception {
         isValid(authToken, gameID);
         connections.add(session, gameID);
-        AuthData auth = authAccess.getAuth(authToken);
-        String username = auth.username();
+        String username = getUsername(authToken);
         ChessGame.TeamColor teamColor;
         String notify;
         if (username.equals(gameAccess.getGame(gameID).whiteUsername())){
@@ -79,8 +58,7 @@ public class WebSocketService {
 
     public void makeMove(int gameID, String authToken, ChessMove move, Session session) throws Exception{
         isValid(authToken, gameID);
-        AuthData auth = authAccess.getAuth(authToken);
-        String username = auth.username();
+        String username = getUsername(authToken);
         GameData gameData = gameAccess.getGame(gameID);
         ChessGame game = gameData.game();
         Collection<ChessMove> validMoves = game.validMoves(move.getStartPosition());
@@ -91,13 +69,80 @@ public class WebSocketService {
         gameAccess.updateGame(gameID, gameData.whiteUsername(), gameData.blackUsername(), gameData.gameName(), game);
         ServerMessage loadGame = new ServerMessage(game);
         connections.broadcast(null, loadGame);
-        String notify = username + "moved their " +
-                game.getBoard().getPiece(move.getStartPosition()).getPieceType() + "to " + moveTranslator(move);
+        String notify = username + " moved their " +
+                game.getBoard().getPiece(move.getStartPosition()).getPieceType() + " to " + moveTranslator(move);
         ServerMessage notification = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION, notify);
         connections.broadcast(session, notification);
+        checkConditions(gameData, ChessGame.TeamColor.WHITE);
+        checkConditions(gameData, ChessGame.TeamColor.BLACK);
     }
 
-    public void leave(int gameID, String authToken, Session session){
-
+    public void leave(int gameID, String authToken, Session session) throws Exception{
+        isValid(authToken, gameID);
+        GameData gameData = gameAccess.getGame(gameID);
+        String username = getUsername(authToken);
+        if (gameData.whiteUsername().equals(username)){
+            gameAccess.updateGame(gameID, null, gameData.blackUsername(), gameData.gameName(), gameData.game());
+        }
+        else if (gameData.blackUsername().equals(username)){
+            gameAccess.updateGame(gameID, gameData.whiteUsername(), null, gameData.gameName(), gameData.game());
+        }
+        ServerMessage notify = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION, username + " has left the game.");
+        connections.remove(session, gameID);
+        connections.broadcast(session, notify);
     }
+
+
+
+    private String getUsername(String authToken) throws Exception{
+        AuthData auth = authAccess.getAuth(authToken);
+        return auth.username();
+    }
+
+    private String moveTranslator(ChessMove move){
+        String[] letters = { "A", "B", "C" ,"D", "E", "F", "G", "H"};
+        int row = move.getEndPosition().getRow();
+        int col = move.getEndPosition().getColumn();
+        return letters[col-1] + row;
+    }
+
+    private void isValid(String authToken, int gameID) throws Exception{
+        if (authAccess.getAuth(authToken) == null){
+            throw new DataAccessException("You are not authorized, please login.");
+        }
+        if (gameAccess.getGame(gameID) == null){
+            throw new DataAccessException("This game does not exist. Please pick a different game.");
+        }
+    }
+
+    private boolean moveIsValid(Collection<ChessMove> validMoves, ChessMove move){
+        return validMoves.contains(move);
+    }
+
+    private void checkConditions(GameData gameData, ChessGame.TeamColor teamColor) throws Exception{
+        String username;
+        ChessGame game = gameData.game();
+        if (teamColor == ChessGame.TeamColor.BLACK){
+            username = gameData.blackUsername();
+        }
+        else{
+           username = gameData.whiteUsername();
+        }
+        if(game.isInCheckmate(teamColor)){
+            ServerMessage notify = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION,
+                    "CHECKMATE! " + username + " has lost.");
+            connections.broadcast(null, notify);
+        }
+        else if(game.isInStalemate(teamColor)){
+            ServerMessage notify = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION,
+                    "STALEMATE! " + username + " is not in check but has no valid moves.");
+            connections.broadcast(null, notify);
+        }
+        else if(game.isInCheck(teamColor)){
+            ServerMessage notify = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION,
+                    username + " is in check!");
+            connections.broadcast(null, notify);
+        }
+    }
+
 }
